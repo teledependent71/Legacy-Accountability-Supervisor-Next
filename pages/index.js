84b38a1158c3647ff35cbc62cb6cb530fc7b4477
teledependent71 <93117032+teledependent71@users.noreply.github.com>
2024-04-18 07:51:56 +0300

    Commit from TeleportHQ

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = (props) => {
+  return (
+    <>
+      <div className="home-container">
+        <Head>
+          <title>Legacy Accountability Supervisor</title>
+          <meta
+            property="og:title"
+            content="Legacy Accountability Supervisor"
+          />
+        </Head>
+      </div>
+      <style jsx>
+        {`
+          .home-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Home
